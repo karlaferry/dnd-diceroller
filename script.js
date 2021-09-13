e586e72abcfd63700e766882f5788cf8e33fb032
Karla Ferry <84235751+karlaferry@users.noreply.github.com>
2021-09-13 05:01:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,92 @@ const d8Dice = document.getElementById('d8')
 const d6Dice = document.getElementById('d6')
 const d4Dice = document.getElementById('d4')
 
-const d20Btn = document.getElementsByClassName('d20-btn')
-const d12Btn = document.getElementsByClassName('d12-btn')
-const d10Btn = document.getElementsByClassName('d10-btn')
-const d8Btn = document.getElementsByClassName('d8-btn')
-const d6Btn = document.getElementsByClassName('d6-btn')
-const d4Btn = document.getElementsByClassName('d4-btn')
+const d20Btn = document.getElementById('d20-btn')
+const d12Btn = document.getElementById('d12-btn')
+const d10Btn = document.getElementById('d10-btn')
+const d8Btn = document.getElementById('d8-btn')
+const d6Btn = document.getElementById('d6-btn')
+const d4Btn = document.getElementById('d4-btn')
+const resetBtn = document.getElementById('reset')
 
-d20Btn[0].addEventListener('click', function(){
-    alert('clicked!')
+function rollDice(dice, sides) {
+    dice.textContent = Math.floor(Math.random()*sides) + 1
+}
+
+d20Btn.addEventListener('click', function(){
+    rollDice(d20Dice, 20)
+    d20Dice.classList.add('active')
+    d12Dice.classList.remove('active')
+    d10Dice.classList.remove('active')
+    d8Dice.classList.remove('active')
+    d6Dice.classList.remove('active')
+    d4Dice.classList.remove('active')
+})
+
+
+d12Btn.addEventListener('click', function() {
+    rollDice(d12Dice, 12)
+    d20Dice.classList.remove('active')
+    d12Dice.classList.add('active')
+    d10Dice.classList.remove('active')
+    d8Dice.classList.remove('active')
+    d6Dice.classList.remove('active')
+    d4Dice.classList.remove('active')
+})
+
+d10Btn.addEventListener('click', function() {
+    rollDice(d10Dice, 10)
+    d20Dice.classList.remove('active')
+    d12Dice.classList.remove('active')
+    d10Dice.classList.add('active')
+    d8Dice.classList.remove('active')
+    d6Dice.classList.remove('active')
+    d4Dice.classList.remove('active')
+})
+
+d8Btn.addEventListener('click', function() {
+    rollDice(d8Dice, 8)
+    d20Dice.classList.remove('active')
+    d12Dice.classList.remove('active')
+    d10Dice.classList.remove('active')
+    d8Dice.classList.add('active')
+    d6Dice.classList.remove('active')
+    d4Dice.classList.remove('active')
+})
+
+d6Btn.addEventListener('click', function() {
+    rollDice(d6Dice, 6)
+    d20Dice.classList.remove('active')
+    d12Dice.classList.remove('active')
+    d10Dice.classList.remove('active')
+    d8Dice.classList.remove('active')
+    d6Dice.classList.add('active')
+    d4Dice.classList.remove('active')
+})
+
+d4Btn.addEventListener('click', function() {
+    rollDice(d4Dice, 4)
+    d20Dice.classList.remove('active')
+    d12Dice.classList.remove('active')
+    d10Dice.classList.remove('active')
+    d8Dice.classList.remove('active')
+    d6Dice.classList.remove('active')
+    d4Dice.classList.add('active')
+})
+
+resetBtn.addEventListener('click', function() {
+    d20Dice.classList.remove('active')
+    d12Dice.classList.remove('active')
+    d10Dice.classList.remove('active')
+    d8Dice.classList.remove('active')
+    d6Dice.classList.remove('active')
+    d4Dice.classList.remove('active')
+
+    d20Dice.textContent = 0
+    d12Dice.textContent = 0
+    d10Dice.textContent = 0
+    d8Dice.textContent = 0
+    d6Dice.textContent = 0
+    d4Dice.textContent = 0
 })
 
