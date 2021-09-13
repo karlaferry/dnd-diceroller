a153e9de7ec695317cc23024f1b8364f202c8fce
Karla Ferry <84235751+karlaferry@users.noreply.github.com>
2021-09-13 04:18:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const d20Dice = document.getElementById('d20')
+const d12Dice = document.getElementById('d12')
+const d10Dice = document.getElementById('d10')
+const d8Dice = document.getElementById('d8')
+const d6Dice = document.getElementById('d6')
+const d4Dice = document.getElementById('d4')
+
+const d20Btn = document.getElementsByClassName('d20-btn')
+const d12Btn = document.getElementsByClassName('d12-btn')
+const d10Btn = document.getElementsByClassName('d10-btn')
+const d8Btn = document.getElementsByClassName('d8-btn')
+const d6Btn = document.getElementsByClassName('d6-btn')
+const d4Btn = document.getElementsByClassName('d4-btn')
+
+d20Btn[0].addEventListener('click', function(){
+    alert('clicked!')
+})
+
